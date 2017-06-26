a65f4f517290e62601b293a34e5560686b00e50b
weiruizhe <806944687@qq.com>
2017-06-26 23:59:55 +0800

    a

--- client/index.js
+++ client/index.js
@@ -2,14 +2,17 @@ var request = require('request');
 var fs = require('fs');
 var path = require('path');
 
-function list(ways, handleFiles, floor) {  
+function list(ways, excludes, handleFiles, floor) {  
     floor = floor || 0;
     handleFiles(ways, ++floor);  
     fs.readdir(ways, function(err, files) {  
         if (err) {  
             console.log('readdir error');  
         } else {  
-            files.forEach(function(item) {  
+            files.forEach(function(item) {
+                if(item.indexOf(excludes) >= 0){
+                    return false;
+                }
                 var tmpPath = ways + '/' + item;  
                 fs.stat(tmpPath, function(err1, stats) {  
                     if (err1) {  
@@ -29,8 +32,8 @@ function list(ways, handleFiles, floor) {
 }  
 
 
-module.exports = function(from,to){
-    list(from,function(ways,floor){    
+module.exports = function(from,to,excludes){
+    list(from,excludes,function(ways,floor){    
         fs.stat(ways, function(err, stats) {
             if(stats.isFile()){
                 var form = request.post('http://st01-rdqa04-dev121-weiruizhe.epc.baidu.com:8999/receiver').form();
--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 var client = require('./client');
+/*
+1.上传目录
+2.目标目录
+3.排除的目录，多个目录用空格分隔
 
-client('test','../../odp/webroot/audiobook/telecontroll');
+目标目录+上传目录=服务器生成的目录
+*/
+client('test','../../odp/webroot/audiobook/telecontroll','.git');
